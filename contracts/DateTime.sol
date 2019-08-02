3591fc43e4580b74088d106d9707566dc6dab4c8
BCDev <bcdev@192-168-1-103.tpgi.com.au>
2019-08-02 17:20:03 +1000

    modified:   contracts/DateTime.sol change contract to library

--- contracts/DateTime.sol
+++ contracts/DateTime.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract DateTime {
+library DateTime {
         /*
          *  Date and Time utilities for ethereum contracts
          *  Updating to version 0.5.0;
